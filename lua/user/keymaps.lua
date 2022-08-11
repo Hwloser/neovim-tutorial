2e810e1cdfc64357b1268c0f286d3709a2c444fb
huanwei <hwloser@163.com>
2022-08-11 17:34:40 +0800

    change the keymaps in mocos

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -10,6 +10,9 @@ keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+keymap("n","<Space>h",":noh<CR>",opts)
+keymap("n","<Space>q",":qa<CR>",opts)
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
@@ -28,14 +31,15 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+-- not effect in macOS
+-- keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+-- keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+-- keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+-- keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "gn", ":bnext<CR>", opts)
+keymap("n", "gb", ":bprevious<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
